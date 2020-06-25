61b9eaa11080857946d006141ae59d3134465075
Adam Barrett <adam.barrett.ext@bayer.com>
2020-06-25 16:13:47 -0600

    updated for bools

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,29 +9,28 @@ while (( "$#" )); do
   if [[ -n $1 ]]; then
     case $arg_pos in
       1) ARGUMENTS+="-b ${1} ";;
-      2) ARGUMENTS+="-D ${1} ";;
-      3) ARGUMENTS+="-E ${1} ";;
-      4) ARGUMENTS+="-F ${1} ";;
+      2) if [[ "true" == "$1" ]]; then ARGUMENTS+="-D "; fi;;
+      3) if [[ "true" == "$1" ]]; then ARGUMENTS+="-E "; fi;;
+      4) if [[ "true" == "$1" ]]; then ARGUMENTS+="-F "; fi;;
       5) ARGUMENTS+="--config ${1} ";;
       6) ARGUMENTS+="--configDir ${1} ";;
-      7) ARGUMENTS+="--configDir ${1} ";;
-      8) ARGUMENTS+="-c ${1} ";;
-      9) ARGUMENTS+="--debug ${1} ";;
-      10) ARGUMENTS+="--enableGitInfo ${1} ";;
-      11) ARGUMENTS+="--environment ${1} ";;
-      12) ARGUMENTS+="--i18n-warnings ${1} ";;
-      13) ARGUMENTS+="--ignoreVendor ${1} ";;
-      14) ARGUMENTS+="-l ${1} ";;
-      15) ARGUMENTS+="--minify ${1} ";;
-      16) ARGUMENTS+="--noChmod ${1} ";;
-      17) ARGUMENTS+="--noTimes ${1} ";;
-      18) ARGUMENTS+="--path-warnings ${1} ";;
-      19) ARGUMENTS+="-s ${1} ";;
-      20) ARGUMENTS+="--templateMetrics ${1} ";;
-      21) ARGUMENTS+="--templateMetricsHints ${1} ";;
-      22) ARGUMENTS+="-t ${1} ";;
-      23)ARGUMENTS+="--themesDir ${1} ";;
-      24) ARGUMENTS+="-v ${1} ";;
+      7) ARGUMENTS+="-c ${1} ";;
+      8) if [[ "true" == "$1" ]]; then ARGUMENTS+="--debug "; fi;;
+      9) if [[ "true" == "$1" ]]; then ARGUMENTS+="--enableGitInfo "; fi;;
+      10) ARGUMENTS+="--environment ${1} ";;
+      11) if [[ "true" == "$1" ]]; then ARGUMENTS+="--i18n-warnings ${1} "; fi;;
+      12) if [[ "true" == "$1" ]]; then ARGUMENTS+="--ignoreVendor ${1} "; fi;;
+      13) ARGUMENTS+="-l ${1} ";;
+      # 14) if [[ "true" == "$1" ]]; then ARGUMENTS+="--minify "; fi;;
+      15) if [[ "true" == "$1" ]]; then ARGUMENTS+="--noChmod "; fi;;
+      16) if [[ "true" == "$1" ]]; then ARGUMENTS+="--noTimes "; fi;;
+      17) if [[ "true" == "$1" ]]; then ARGUMENTS+="--path-warnings "; fi ;;
+      18) ARGUMENTS+="-s ${1} ";;
+      19) if [[ "true" == "$1" ]]; then ARGUMENTS+="--templateMetrics "; fi;;
+      20) if [[ "true" == "$1" ]]; then ARGUMENTS+="--templateMetricsHints "; fi;;
+      21) ARGUMENTS+="-t ${1} ";;
+      22) ARGUMENTS+="--themesDir ${1} ";;
+      23) if [[ "true" == "$1" ]]; then ARGUMENTS+="-v"; fi;;
     esac
   fi
   arg_pos=$((arg_pos+1))
@@ -42,6 +41,6 @@ done
   if [ -n "${GITHUB_ACTIONS}" ]; then
     cd "${GITHUG_WORKSPACE}";
   fi;
-  echo "hugo ${ARGUMENTS}"
+  echo "\"hugo ${ARGUMENTS}\""
   ../hugo "${ARGUMENTS}"
 )
