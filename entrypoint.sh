ed0f59f1428ccbdfcaef501e9bdf0f2de2987827
Adam Barrett <adam.barrett.ext@bayer.com>
2020-06-25 15:45:26 -0600

    added support for all inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,47 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-# Get into the workspace
-cd "${GITHUB_WORKSPACE}";
+ARGUMENTS=""
 
-# Show what is here before we run hugo
-ls -A;
+# Handle Arguments
+arg_pos=1
 
-# Run hugo
-hugo;
+while (( "$#" )); do
+  if [[ -n $1 ]]; then
+    case $arg_pos in
+      1) ARGUMENTS+="-b ${1} ";;
+      2) ARGUMENTS+="-D ${1} ";;
+      3) ARGUMENTS+="-E ${1} ";;
+      4) ARGUMENTS+="-F ${1} ";;
+      5) ARGUMENTS+="--config ${1} ";;
+      6) ARGUMENTS+="--configDir ${1} ";;
+      7) ARGUMENTS+="--configDir ${1} ";;
+      8) ARGUMENTS+="-c ${1} ";;
+      9) ARGUMENTS+="--debug ${1} ";;
+      10) ARGUMENTS+="--enableGitInfo ${1} ";;
+      11) ARGUMENTS+="--environment ${1} ";;
+      12) ARGUMENTS+="--i18n-warnings ${1} ";;
+      13) ARGUMENTS+="--ignoreVendor ${1} ";;
+      14) ARGUMENTS+="-l ${1} ";;
+      15) ARGUMENTS+="--minify ${1} ";;
+      16) ARGUMENTS+="--noChmod ${1} ";;
+      17) ARGUMENTS+="--noTimes ${1} ";;
+      18) ARGUMENTS+="--path-warnings ${1} ";;
+      19) ARGUMENTS+="-s ${1} ";;
+      20) ARGUMENTS+="--templateMetrics ${1} ";;
+      21) ARGUMENTS+="--templateMetricsHints ${1} ";;
+      22) ARGUMENTS+="-t ${1} ";;
+      23)ARGUMENTS+="--themesDir ${1} ";;
+      24) ARGUMENTS+="-v ${1} ";;
+    esac
+  fi
+  arg_pos=$((arg_pos+1))
+  shift
+done
 
-# Show what is here after running hugo
-ls -A;
+(
+  if [ -n "${GITHUB_ACTIONS}" ]; then
+    cd "${GITHUG_WORKSPACE}";
+  fi;
+  echo "hugo ${ARGUMENTS}"
+  ../hugo "${ARGUMENTS}"
+)
