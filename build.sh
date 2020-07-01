1c4bc88f3f8ba7e133b20daf2c037f7f26072306
Adam Barrett <adam.barrett.ext@bayer.com>
2020-07-01 12:28:40 -0600

    removing submodule dependency (maybe)

--- build.sh
+++ build.sh
@@ -8,36 +8,36 @@ mkdir "${BUILD_DIR}"
 
 LATEST=$(
   echo "Copying to Build Dir: ${BUILD_DIR}" >&2
-  cp -r . "${BUILD_DIR}"
+  cp -r . "${BUILD_DIR}" >&2
   cd "${BUILD_DIR}" || exit 1
 
   echo "Updating Submodules" >&2
-  git submodule foreach git pull origin master
+  git submodule foreach git pull origin master >&2
 
   echo "Installing Hugo" >&2
   LATEST=$(
     cd hugo || exit
-    LATEST=$(git tag | sort -rV | head -n 1)
-    git checkout "${LATEST}"
-    go install --tags extended
-    mv "${HOME}"/go/bin/hugo .
+    LATEST=$(git tag | sort -rV | head -n 1) >&2
+    git checkout "${LATEST}" >&2
+    go install --tags extended >&2
+    mv "${HOME}"/go/bin/hugo . >&2
     echo "${LATEST}"
   )
   echo "Installed version: ${LATEST}" >&2
 
   echo "Creating action.yml" >&2
-  sed "s/%%version%%/${LATEST}/" action.yml.tmp >action.yml
+  sed "s/%%version%%/${LATEST}/" action.yml.tmp >action.yml >&2
 
   echo "Commit new action.yml" >&2
-  git add action.yml
-  git commit -m "Updating action.yml with version ${LATEST}"
-  git push origin :refs/tags/${LATEST}
-  git push origin :refs/tags/latest
-  git tag -af "${LATEST}" -m "Version ${LATEST}"
-  git tag -af latest -m "Version ${LATEST}"
-  git push --tags
+  git checkout -b ${LATEST} >&2
+  git add action.yml >&2
+  git commit -m "Updating action.yml with version ${LATEST}" >&2
+  git push origin :refs/tags/latest  >&2
+  git tag -af latest -m "Version ${LATEST}"  >&2
+  git push origin ${LATEST} --tags >&2
+
   echo "${LATEST}"
 )
 
 echo "Building Container"
-podman build . -t utahcon/hugo:${LATEST}
+podman build . -t utahcon/hugo:"${LATEST}"
