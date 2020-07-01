d84234cad624905a81ce31902a2b74198fab0be6
Adam Barrett <adam.barrett.ext@bayer.com>
2020-07-01 11:10:43 -0600

    new build steps

--- build.sh
+++ build.sh
@@ -2,16 +2,42 @@
 
 echo "Hugo Builder"
 
-echo "Updating Submodules"
-git submodule foreach git pull origin master
+UUID=$(uuidgen)
+BUILD_DIR=/tmp/build_${UUID}
+mkdir "${BUILD_DIR}"
 
-echo "Installing Hugo"
-(
-  cd hugo || exit;
-  go install --tags extended
-  cp "${HOME}"/go/bin/hugo .
-  ./hugo version
+LATEST=$(
+  echo "Copying to Build Dir: ${BUILD_DIR}" >&2
+  cp -r . "${BUILD_DIR}"
+  cd "${BUILD_DIR}" || exit 1
+
+  echo "Updating Submodules" >&2
+  git submodule foreach git pull origin master
+
+  echo "Installing Hugo" >&2
+  LATEST=$(
+    cd hugo || exit
+    LATEST=$(git tag | sort -rV | head -n 1)
+    git checkout "${LATEST}"
+    go install --tags extended
+    mv "${HOME}"/go/bin/hugo .
+    echo "${LATEST}"
+  )
+  echo "Installed version: ${LATEST}" >&2
+
+  echo "Creating action.yml" >&2
+  sed "s/%%version%%/${LATEST}/" action.yml.tmp >action.yml
+
+  echo "Commit new action.yml" >&2
+  git add action.yml
+  git commit -m "Updating action.yml with version ${LATEST}"
+  git push origin :refs/tags/${LATEST}
+  git push origin :refs/tags/latest
+  git tag -af "${LATEST}" -m "Version ${LATEST}"
+  git tag -af latest -m "Version ${LATEST}"
+  git push --tags
+  echo "${LATEST}"
 )
 
 echo "Building Container"
-podman build . -t utahcon/hugo:local
+podman build . -t utahcon/hugo:${LATEST}
