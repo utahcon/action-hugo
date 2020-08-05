9e14d0a65bbd83c7536be86be303620fe02ee40d
Adam Barrett <adam.barrett.ext@bayer.com>
2020-08-05 12:47:49 -0600

    build changes

--- hugo-scraper.sh
+++ hugo-scraper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-# scrape the help info from Hugo to create better action.yml
+set -euo pipefail
+# scrape the help info from Hugo to create better action.yml and entrypoint.sh
 
 # left trimming!
 function trim_left {
@@ -8,7 +8,45 @@ function trim_left {
   echo "${1}" | sed -e "s/^\s*//"
 }
 
-INPUTS=""
-
+YML_INPUTS=""
+YML_ARGS=""
+ENTRY=""
 IFS=$'\n'
 
+COUNT=1
+
+for FLAG in $(hugo --help | grep "^\s*-.*"); do
+#  echo -e "\n"
+  FLAG=$(trim_left "${FLAG}")
+#  echo "'${FLAG}'"
+
+  # Get Short Flag
+  SHORT=${FLAG%%--*}
+  SHORT=$(trim_left "${SHORT}")
+#  echo "Short:1:2 ${SHORT:1:1}"
+  FLAG=${FLAG#*--}
+
+  # Get Long Flag
+  FLAG=$(trim_left "${FLAG}")
+  LONG=${FLAG%% *}
+  LONG=$(trim_left "${LONG}")
+#  echo "Long: '${LONG}'"
+
+  FLAG=${FLAG#* }
+  ARG=${FLAG%% *}
+#  echo "Arg: '${ARG}'"
+
+  FLAG=${FLAG#* }
+  DESC=$(trim_left "${FLAG}")
+#  echo "Desc: '${DESC}'"
+
+  YML_INPUTS+="\n  ${LONG}\:\n    description\: ${DESC}\n    required\: false"
+  YML_ARGS+="\n    - \${{ inputs.${LONG} }}"
+  ENTRY+="\n      ${COUNT}) ARGUMENTS+=\"--${LONG} \${1} \";;"
+
+  ((COUNT=COUNT+1))
+done
+
+sed -e "s;%%INPUTS%%;${YML_INPUTS};" action.yml.tmp > action.yml.tmp2
+sed -e "s/%%ARGS%%/${YML_ARGS}/" action.yml.tmp2 > action.yml
+sed -e "s/%%ENTRY%%/${ENTRY}/" entrypoint.sh.tmp > entrypoint.sh
