c55fe05fcf2eb26df0ef352d0140f772d3cabfe0
Adam Barrett <adam.barrett.ext@bayer.com>
2020-09-17 10:38:42 -0600

    Adding second push

--- workflow_mermaid.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open(".github/workflows/build.yml") as file:
-    workflow = yaml.load(file)
-
-    for key, value in workflow.items():
-        if key is True:
-            print(value)
-            print(key)
